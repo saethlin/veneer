11e8a0211d8c23086decdb4c53f6fa68abd1b988
Ben Kimock <kimockb@gmail.com>
2020-09-18 22:40:31 -0400

    more syscalls

--- src/syscalls.rs
+++ src/syscalls.rs
@@ -1,5 +1,5 @@
 use crate::{CStr, Error};
-
+use core::{marker::PhantomData, mem};
 use libc::c_int;
 use sc::syscall;
 
@@ -46,21 +46,21 @@ pub fn close(fd: c_int) -> Result<(), Error> {
 
 pub fn stat(path: CStr) -> Result<libc::stat, Error> {
     unsafe {
-        let mut status: libc::stat = core::mem::zeroed();
+        let mut status: libc::stat = mem::zeroed();
         syscall!(STAT, path.as_ptr(), &mut status as *mut libc::stat).to_result_with(status)
     }
 }
 
 pub fn fstat(fd: c_int) -> Result<libc::stat, Error> {
     unsafe {
-        let mut status: libc::stat = core::mem::zeroed();
+        let mut status: libc::stat = mem::zeroed();
         syscall!(FSTAT, fd, &mut status as *mut libc::stat).to_result_with(status)
     }
 }
 
 pub fn lstat(path: CStr) -> Result<libc::stat, Error> {
     unsafe {
-        let mut status: libc::stat = core::mem::zeroed();
+        let mut status: libc::stat = mem::zeroed();
         syscall!(FSTAT, path.as_ptr(), &mut status as *mut libc::stat).to_result_with(status)
     }
 }
@@ -119,7 +119,7 @@ pub fn sigaction(
             signal,
             action as *const libc::sigaction,
             old_action as *mut libc::sigaction,
-            core::mem::size_of::<libc::sigset_t>()
+            mem::size_of::<libc::sigset_t>()
         )
     }
     .to_result_with(())
@@ -147,17 +147,63 @@ pub fn pwrite64(fd: c_int, buf: &[u8], offset: usize) -> Result<usize, Error> {
     unsafe { syscall!(PWRITE64, fd, buf.as_ptr(), buf.len(), offset) }.usize_result()
 }
 
-// readv
-//
-// writev
-//
-// access
-//
-// pipe
-//
-// select
-//
-// sched_yield
+pub struct IoVec<'a> {
+    #[allow(dead_code)]
+    iov_base: *mut u8,
+    #[allow(dead_code)]
+    iov_len: usize,
+    _danny: PhantomData<&'a mut u8>,
+}
+
+pub fn readv<'a, 'b>(fd: c_int, iovec: &'b mut [IoVec<'a>]) -> Result<usize, Error> {
+    unsafe { syscall!(READV, fd, iovec.as_mut_ptr(), iovec.len()) }.usize_result()
+}
+
+pub fn writev<'a, 'b>(fd: c_int, iovec: &'b [IoVec<'a>]) -> Result<usize, Error> {
+    unsafe { syscall!(READV, fd, iovec.as_ptr(), iovec.len()) }.usize_result()
+}
+
+bitflags::bitflags! {
+    pub struct Mode: c_int {
+        const F_OK = 0;
+        const R_OK = 4;
+        const W_OK = 2;
+        const X_OK = 1;
+    }
+}
+
+pub fn access(pathname: CStr, mode: Mode) -> Result<(), Error> {
+    unsafe { syscall!(ACCESS, pathname.as_ptr(), mode.bits()) }.null_result()
+}
+
+pub fn pipe() -> Result<[c_int; 2], Error> {
+    let mut pipefd: [c_int; 2] = [0, 0];
+    unsafe { syscall!(PIPE, pipefd.as_mut_ptr()) }.to_result_with(pipefd)
+}
+
+pub fn select(
+    nfds: c_int,
+    readfds: &mut libc::fd_set,
+    writefds: &mut libc::fd_set,
+    exceptfds: &mut libc::fd_set,
+    timeout: &mut libc::timespec,
+) -> Result<c_int, Error> {
+    unsafe {
+        syscall!(
+            SELECT,
+            nfds,
+            readfds as *mut libc::fd_set,
+            writefds as *mut libc::fd_set,
+            exceptfds as *mut libc::fd_set,
+            timeout as *mut libc::timespec
+        )
+    }
+    .to_result_and(|n| n as c_int)
+}
+
+pub fn sched_yield() -> Result<(), Error> {
+    unsafe { syscall!(SCHED_YIELD) }.null_result()
+}
 
 pub fn mremap(
     old_address: *mut u8,
@@ -169,7 +215,133 @@ pub fn mremap(
         .to_result_and(|n| n as *mut u8)
 }
 
-// Require that it non-negative
+bitflags::bitflags! {
+    pub struct MSync: c_int {
+        const ASYNC = libc::MS_ASYNC;
+        const SYNC = libc::MS_SYNC;
+        const INVALIDATE = libc::MS_INVALIDATE;
+    }
+}
+
+pub fn msync(memory: &[u8], flags: MSync) -> Result<(), Error> {
+    unsafe { syscall!(MSYNC, memory.as_ptr(), memory.len(), flags.bits()) }.null_result()
+}
+
+pub fn mincore(memory: &[u8], status: &mut [u8]) -> Result<(), Error> {
+    if status.len() < (memory.len() + 4096 - 1) / 4096 {
+        return Err(Error(libc::EINVAL as isize));
+    }
+    unsafe { syscall!(MINCORE, memory.as_ptr(), memory.len(), status.as_mut_ptr()) }.null_result()
+}
+
+bitflags::bitflags! {
+    pub struct Advice: c_int {
+        const NORMAL = libc::MADV_NORMAL;
+        const RANDOM = libc::MADV_RANDOM;
+        const SEQUENTIAL = libc::MADV_SEQUENTIAL;
+        const WILLNEED = libc::MADV_WILLNEED;
+        const DONTNEED = libc::MADV_DONTNEED;
+        const REMOVE = libc::MADV_REMOVE;
+        const DONTFORM = libc::MADV_DONTFORK;
+        const DOFORK = libc::MADV_DOFORK;
+        const HWPOISON = libc::MADV_HWPOISON;
+        const MERGEABLE = libc::MADV_MERGEABLE;
+        const UNMERGEABLE = libc::MADV_UNMERGEABLE;
+        const SOFT_OFFLINE = libc::MADV_SOFT_OFFLINE;
+        const HUGEPAGE = libc::MADV_HUGEPAGE;
+        const NOHUGEPAGE = libc::MADV_NOHUGEPAGE;
+        const DONTDUMP = libc::MADV_DONTDUMP;
+        const DODUMP = libc::MADV_DODUMP;
+        const FREE = libc::MADV_FREE;
+        //const WIPEONFORK = libc::MADV_WIPEONFORK;
+        //const KEEPONFORK = libc::MADV_KEEPONFORK;
+    }
+}
+
+pub fn madvise(memory: &[u8], advice: Advice) -> Result<(), Error> {
+    unsafe { syscall!(MADVISE, memory.as_ptr(), memory.len(), advice.bits()) }.null_result()
+}
+
+bitflags::bitflags! {
+    pub struct ShmFlags: c_int {
+        const CREAT = libc::IPC_CREAT;
+        const EXCL = libc::IPC_EXCL;
+        const HUGETLB = libc::SHM_HUGETLB;
+        const HUGE_2MB = libc::MAP_HUGE_2MB;
+        const HUGE_1GB = libc::MAP_HUGE_1GB;
+        const NORESERVE = libc::SHM_NORESERVE;
+    }
+}
+
+pub fn shmget(key: libc::key_t, size: usize, shmflg: ShmFlags) -> Result<libc::key_t, Error> {
+    unsafe { syscall!(SHMGET, key, size, shmflg.bits()) }.to_result_and(|key| key as libc::key_t)
+}
+
+// shmctl
+//
+// dup
+//
+// dup2
+//
+// pause
+//
+// nanosleep
+//
+// getitimer
+//
+// alarm
+//
+// setitimer
+//
+// getpid
+//
+// sendfile
+//
+// socket
+//
+// connect
+//
+// accept
+//
+// sendto
+//
+// recvfrom
+//
+// sendmsg
+//
+// recvmsg
+//
+// shutdown
+//
+// bind
+//
+// listen
+//
+// getsockname
+//
+// getpeername
+//
+// socketpair
+//
+// setsockopt
+//
+// getsockopt
+//
+// clone
+//
+// fork
+//
+// execve
+
+pub fn exit(error_code: c_int) {
+    unsafe {
+        syscall!(EXIT, error_code);
+    }
+}
+
+// wait4
+
+// Require that it is non-negative
 pub struct Pid(libc::pid_t);
 
 pub enum SignalWhere {
@@ -179,18 +351,14 @@ pub enum SignalWhere {
     Group(usize),
 }
 pub fn kill(pid: usize, signal: i32) -> Result<(), Error> {
-    unsafe { syscall!(KILL, pid, signal) }.to_result_with(())
+    unsafe { syscall!(KILL, pid, signal) }.null_result()
 }
 
-pub fn exit(error_code: c_int) {
-    unsafe {
-        syscall!(EXIT, error_code);
-    }
-}
+// uname
 
 pub fn fstatat(fd: c_int, name: CStr) -> Result<libc::stat64, Error> {
     unsafe {
-        let mut stats = core::mem::zeroed();
+        let mut stats = mem::zeroed();
         syscall!(
             NEWFSTATAT,
             fd,
@@ -204,7 +372,7 @@ pub fn fstatat(fd: c_int, name: CStr) -> Result<libc::stat64, Error> {
 
 pub fn lstatat(fd: c_int, name: CStr) -> Result<libc::stat64, Error> {
     unsafe {
-        let mut stats = core::mem::zeroed();
+        let mut stats = mem::zeroed();
         syscall!(
             NEWFSTATAT,
             fd,
@@ -221,7 +389,7 @@ pub fn getdents64(fd: c_int, buf: &mut [u8]) -> Result<usize, Error> {
 }
 
 pub fn faccessat(fd: c_int, name: CStr, mode: c_int) -> Result<(), Error> {
-    unsafe { syscall!(FACCESSAT, fd, name.as_ptr(), mode) }.to_result_with(())
+    unsafe { syscall!(FACCESSAT, fd, name.as_ptr(), mode) }.null_result()
 }
 
 pub fn readlinkat<'a>(fd: c_int, name: CStr, buf: &'a mut [u8]) -> Result<&'a [u8], Error> {
@@ -235,7 +403,7 @@ pub fn readlinkat<'a>(fd: c_int, name: CStr, buf: &'a mut [u8]) -> Result<&'a [u
 
 pub fn winsize() -> Result<libc::winsize, Error> {
     unsafe {
-        let mut winsize: libc::winsize = core::mem::zeroed();
+        let mut winsize: libc::winsize = mem::zeroed();
         syscall!(
             IOCTL,
             libc::STDOUT_FILENO,
