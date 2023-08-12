e1721b7f08409af0f7fc62293a629502d205caa8
Ben Kimock <kimockb@gmail.com>
2023-08-12 11:21:46 -0400

    Segment runtime components into a feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(test), no_std)]
-#![feature(naked_functions, alloc_error_handler, lang_items)]
+#![feature(naked_functions, alloc_error_handler)]
 #![warn(clippy::missing_inline_in_public_items)]
 #![feature(cfg_target_has_atomic, core_intrinsics, inline_const, linkage)]
 
 #[cfg(not(target_os = "linux"))]
-core::compile_error!("This library is only implemented for linux");
+core::compile_error!("This library is only implemented for Linux,\n\
+    because the primary goal of this library is to bypass");
 
 #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
 compile_error!("This crate is only implemented for x86_64 and aarch64");
@@ -29,18 +30,18 @@ pub use cstr::CStr;
 pub use error::Error;
 pub use veneer_macros::main;
 
-#[cfg(not(test))]
+#[cfg(all(feature = "rt", not(test)))]
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn eh_personality() {}
 
-#[cfg(not(test))]
+#[cfg(all(feature = "rt", not(test)))]
 #[alloc_error_handler]
 fn alloc_error(layout: core::alloc::Layout) -> ! {
     panic!("memory allocation of {} bytes failed", layout.size());
 }
 
-#[cfg(not(test))]
+#[cfg(all(feature = "rt", not(test)))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     crate::eprintln!("{}", info);
@@ -48,7 +49,7 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     crate::syscalls::exit(-1)
 }
 
-#[cfg(all(not(test), target_arch = "x86_64"))]
+#[cfg(all(feature = "rt", not(test), target_arch = "x86_64"))]
 #[no_mangle]
 #[naked]
 unsafe extern "C" fn _start() {
@@ -63,7 +64,7 @@ unsafe extern "C" fn _start() {
     )
 }
 
-#[cfg(all(not(test), target_arch = "aarch64"))]
+#[cfg(all(feature = "rt", not(test), target_arch = "aarch64"))]
 #[no_mangle]
 #[naked]
 unsafe extern "C" fn _start() {
@@ -77,14 +78,14 @@ unsafe extern "C" fn _start() {
     )
 }
 
-#[cfg(not(test))]
+#[cfg(all(feature = "rt", not(test)))]
 #[no_mangle]
 unsafe extern "C" fn __veneer_init(argc: isize, argv: *mut *const u8) {
     crate::env::ARGC.store(argc, core::sync::atomic::Ordering::SeqCst);
     crate::env::ARGV.store(argv.cast(), core::sync::atomic::Ordering::SeqCst);
 }
 
-#[cfg(not(test))]
+#[cfg(all(feature = "rt", not(test)))]
 #[global_allocator]
 static ALLOC: crate::Allocator = crate::Allocator::new();
 
